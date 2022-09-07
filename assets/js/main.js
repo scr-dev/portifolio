7982e3988983e38e213a27faf1ccc496b73a464c
Sidnei Cerqueira <sidnei.c.rodrigues@hotmail.com>
2022-09-07 04:28:31 -0300

    added the header and your styles sheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,32 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.querySelector('#nav-menu'),
+      navToggle = document.querySelector('#nav-toggle'),
+      navClose = document.querySelector('#nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })    
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){ 
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 /*==================== ACCORDION SKILLS ====================*/
