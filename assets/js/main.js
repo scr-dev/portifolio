12da793d93b6fc4d056ad6e6d9302f63fbd273f4
Sidnei Cerqueira <sidnei.c.rodrigues@hotmail.com>
2022-09-12 22:42:30 -0300

    added portfolio and testimonial sessions

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,7 @@ modalClose.forEach((modalClose) =>{
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPrtifolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
 
@@ -112,6 +112,23 @@ let swiper = new Swiper(".portfolio__container", {
 
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+});
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
